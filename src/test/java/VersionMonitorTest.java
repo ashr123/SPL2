d84a3fc74db23d42f61f82a2690c1e6d6a0211dd
Roy <roykid123@gmail.com>
2017-12-05 18:13:33 +0200

    dsfdsf

---
+++ src/test/java/VersionMonitorTest.java
@@ -0,0 +1,23 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VersionMonitorTest
+{
+	@Test
+	void getVersion()
+	{
+	}
+
+	@Test
+	void inc()
+	{
+	}
+
+	@Test
+	void await()
+	{
+	}
+}
