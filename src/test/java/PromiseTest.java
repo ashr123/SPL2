79d3fedc02c7aac57706957830fdf2697f560d8d
Roy <roykid123@gmail.com>
2017-12-05 18:44:10 +0200

    cvxv

---
+++ src/test/java/PromiseTest.java
@@ -0,0 +1,28 @@
+package test.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PromiseTest
+{
+	@Test
+	public void get()
+	{
+	}
+
+	@Test
+	public void isResolved()
+	{
+	}
+
+	@Test
+	public void resolve()
+	{
+	}
+
+	@Test
+	public void subscribe()
+	{
+	}
+}
---
+++ src/test/java/VersionMonitorTest.java
@@ -0,0 +1,23 @@
+package test.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VersionMonitorTest
+{
+	@Test
+	public void getVersion()
+	{
+	}
+
+	@Test
+	public void inc()
+	{
+	}
+
+	@Test
+	public void await()
+	{
+	}
+}
