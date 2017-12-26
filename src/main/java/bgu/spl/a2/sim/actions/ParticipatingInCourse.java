cd3b84caed0f89926da3a2ee70ea428307e5ff0d
torenda <torenda@post.bgu.ac.il>
2017-12-26 19:13:36 +0200

    hhh

--- src/main/java/bgu/spl/a2/sim/actions/ParticipatingInCourse.java
+++ src/main/java/bgu/spl/a2/sim/actions/ParticipatingInCourse.java
@@ -1,6 +1,11 @@
 package bgu.spl.a2.sim.actions;
 
 import bgu.spl.a2.Action;
+import bgu.spl.a2.sim.privateStates.CoursePrivateState;
+import bgu.spl.a2.sim.privateStates.StudentPrivateState;
+
+import java.util.Collection;
+import java.util.LinkedList;
 
 /**
  * Behavior: This action should try to register the student in the course, if it succeeds, should add the
@@ -10,16 +15,98 @@ import bgu.spl.a2.Action;
  */
 public class ParticipatingInCourse extends Action<Boolean>
 {
-	public ParticipatingInCourse()//TODO Change constructor's signature
+	private String student;
+	private int grade;
+
+	public ParticipatingInCourse(String student,int grade)
 	{
-		//TODO: replace method body with real implementation
-		throw new UnsupportedOperationException("Not Implemented Yet.");
+		setActionName("Participate In Course");
+		this.student=student;
+		this.grade=grade;
 	}
 
 	@Override
 	protected void start()
 	{
-		//TODO: replace method body with real implementation
-		throw new UnsupportedOperationException("Not Implemented Yet.");
+		if(actorState instanceof CoursePrivateState &&
+		   actorThreadPool.getPrivateState(student) instanceof StudentPrivateState)
+		{
+			if(((CoursePrivateState)actorState).getAvailableSpots()!=-1)
+			{
+				Boolean canRegister=true;
+				for(String course : ((CoursePrivateState)actorState).getPrequisites())
+				{
+					if(((StudentPrivateState)actorThreadPool.getPrivateState(student))
+							   .getGrades().get(course)!=null &&
+					   ((StudentPrivateState)actorThreadPool.getPrivateState(student))
+							   .getGrades().get(course)<56)
+						canRegister=false;
+				}
+				if(canRegister)
+				{
+					Collection<Action<?>> actions=new LinkedList<>();
+					int spaces=((CoursePrivateState)actorState).getAvailableSpots();
+					((CoursePrivateState)actorState).setAvailableSpots(spaces-1);
+					Action<Boolean> action=new Action<Boolean>()
+					{
+						@Override
+						protected void start()
+						{
+							if(actorState instanceof StudentPrivateState)
+							{
+								if(((StudentPrivateState)actorState).getGrades().containsKey(ParticipatingInCourse.this.actorID))
+									complete(false);
+								else
+								{
+									((StudentPrivateState)actorState).getGrades().put(ParticipatingInCourse.this.actorID,grade);
+									complete(true);
+								}
+							}
+							else
+								complete(false);
+							synchronized (System.out)
+							{
+								System.out.println("Course "+ParticipatingInCourse.this.actorID+" has "+(getResult().get() ? "SUCCESSFULLY" : "NOT")+" been added to student "+actorID);
+							}
+						}
+					};
+					sendMessage(action, student, new StudentPrivateState());
+					actions.add(action);
+					then(actions, () -> {
+						if(!action.getResult().get())
+						{
+							((CoursePrivateState)actorState).setAvailableSpots(spaces+1);
+							complete(false);
+						}
+						else
+							complete(true);
+					});
+				}
+				else
+				{
+					complete(false);
+					synchronized (System.out)
+					{
+						System.out.println("student "+student+" does not have prequisites for course "+actorID);
+					}
+				}
+			}
+			else
+			{
+				complete(false);
+				synchronized (System.out)
+				{
+					System.out.println("student "+student+" does not have place in course "+actorID);
+				}
+			}
+		}
+		else
+		{
+			complete(false);
+			synchronized (System.out)
+			{
+				System.out.println("student "+student+" has NOT been registered for course "+actorID);
+			}
+		}
 	}
 }
